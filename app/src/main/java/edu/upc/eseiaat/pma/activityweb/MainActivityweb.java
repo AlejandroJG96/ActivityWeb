bd362fa00c86d27f4accca7b87af2a67e42ccb27
AlejandroJG <ajaimezg@hotmail.com>
2017-10-09 12:28:53 +0200

    ActivityWeb

---
+++ app/src/main/java/edu/upc/eseiaat/pma/activityweb/MainActivityweb.java
@@ -0,0 +1,13 @@
+package edu.upc.eseiaat.pma.activityweb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityweb extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_activityweb);
+    }
+}
